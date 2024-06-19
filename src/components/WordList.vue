0c2145fa50845404f405999193c229221d55e225
Martin Vlcek <martin.vlcek@cassoviacode.com>
2024-06-19 14:32:35 +0200

    Word list filter

--- src/components/WordList.vue
+++ src/components/WordList.vue
@@ -9,14 +9,47 @@
         flat
       >
         <template v-slot:text>
-          <v-text-field
-            v-model="search"
-            label="Search"
-            prepend-inner-icon="mdi-magnify"
-            variant="outlined"
-            hide-details
-            single-line
-          ></v-text-field>
+          <v-row align="center" justify="center">
+            <v-col>
+              <v-text-field
+                v-model="search"
+                label="Search"
+                prepend-inner-icon="mdi-magnify"
+                variant="outlined"
+                hide-details
+                single-line
+              ></v-text-field>
+            </v-col>
+
+            <v-col>
+              <v-radio-group
+                class="d-flex"
+                v-model="courseFilter"
+                inline
+              >
+                <v-radio
+                  label="All"
+                  value="all"
+                ></v-radio>
+                <v-radio
+                  label="INT 1"
+                  value="INT 1"
+                ></v-radio>
+                <v-radio
+                  label="INT 2"
+                  value="INT 2"
+                ></v-radio>
+                <v-radio
+                  label="INT 3"
+                  value="INT 3"
+                ></v-radio>
+                <v-radio
+                  label="INT 4"
+                  value="INT 4"
+                ></v-radio>
+              </v-radio-group>
+            </v-col>
+          </v-row>
         </template>
 
         <v-data-table
@@ -42,13 +75,15 @@ import {wordList} from "@/items/word-list";
     { key: 'course', title: 'Course', sortable: false },
   ])
 
-  // const list = ref(
-  //   wordList
-  // )
+  const courseFilter = ref('all')
 
   const wordListItems = computed(() => {
-    // return list.value;
-    return wordList;
+
+    if (courseFilter.value === 'all') {
+      return wordList
+    }
+
+    return wordList.filter(course => course.course.toLowerCase() === courseFilter.value.toLowerCase())
   })
 
 </script>
